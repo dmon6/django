87ae00953b4206643cb62495113eb18cf893f07b
Dennis Monton <dmonton66@gmail.com>
2019-03-14 07:40:54 -0400

    added login/logout templates

--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm#, UserUpdateForm, ProfileUpdateForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -8,9 +10,19 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You are now able to log in {username}!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+@login_required
+def profile(request):
+    #u_form = UserUpdateForm()
+    #p_form = ProfileUpdateForm()
+
+    #context = {
+    #    'u_form': u_form,
+    #    'p_form': p_form
+    #}
+    return render(request, 'users/profile.html', context)
